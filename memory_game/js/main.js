8e9c0c9dc0aef87e7a816ae9a24943793bb10e20
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-08-25 21:10:57 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,19 @@
+var cards = ["queen", "queen" , "king" , "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length = 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match.");
+	}
+	else if(cardsInPlay[0] !== cardsInPlay[1]) {
+	 alert("Sorry, try again.");
+}
+}
+
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
